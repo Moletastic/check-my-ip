c46e1bea8a684b4aa736c4f31040840a9384450c
Jake <boxjack.romero@gmail.com>
2022-08-22 19:51:12 -0400

    feat: extract ip from request headers

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,25 @@ interface HomeProps {
   ip: string;
 }
 
-export function getServerSideProps(_ctx: NextPageContext): { props: HomeProps } {
-  return { props: { ip: 'unknown' } };
+export function getServerSideProps(ctx: NextPageContext): { props: HomeProps } {
+  const props: HomeProps = { ip: "unknown" };
+  const { req } = ctx;
+
+  if (!req) {
+    return { props };
+  }
+
+  if (typeof req.headers["x-forwarded-for"] === "string") {
+    props.ip = req.headers["x-forwarded-for"].split(",")[0];
+    return { props };
+  }
+
+  if (req.headers["x-real-ip"]) {
+    props.ip = req.connection.remoteAddress ?? "unknown";
+    return { props };
+  }
+
+  return { props };
 }
 
 const Home: NextPage<HomeProps> = (props: HomeProps) => {
